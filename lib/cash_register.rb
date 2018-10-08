92788887866ef7caa4414115f472a40b862865af
Alyssa Foxx <alyssamariefoxx@gmail.com>
2018-10-08 01:34:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,9 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total = @total - 
+    if @discount == 0
+      @total = @total - (price * quantity)
+    else @total = @total - ((price * quantity) - @discount / 100)
   end
 
 end
